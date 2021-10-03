b858d62fac4cbada906a5e1fa2da72e477c5c29c
kawsarm104 <kawsarm104@gmail.com>
2021-10-03 22:21:47 +0600

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text-warning">Assignment 9</h1>
     </div>
   );
 }
 
 export default App;
+
+
+// https://github.com/ProgrammingHeroWC4/review-website-kawsarm104.git
