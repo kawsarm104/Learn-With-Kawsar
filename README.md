703f0dcbe825984d002a5e2c5172eda823f1eda4
kawsarm104 <kawsarm104@gmail.com>
2021-10-14 22:09:37 +0600

    updated readme file

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
-### Learn with Kawsar
-## Live Link: https://awesome-wozniak-716a19.netlify.app/ 
+## Learn with Kawsar
 
-* An Educational website where student can see and enroll courses
-* Student can see the course teacher 
-* fully responsive 
+### Live Link: https://awesome-wozniak-716a19.netlify.app/
+
+### Live Link: learn-with-kawsar.web.app
+
+- An Educational website where student can see and enroll courses
+- Student can see the course teacher
+- fully responsive
--- src/components/Login/Login/Login.js
+++ src/components/Login/Login/Login.js
@@ -27,9 +27,9 @@ const Login = () => {
           <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
             <div class="card border-0 shadow rounded-3 my-5">
               <div class="card-body p-4 p-sm-5">
-                <h5 class="card-title text-center mb-5 fw-light fs-5">
-                  Sign In
-                </h5>
+                <h2 class="card-title text-center mb-5 fw-light fs-5 ">
+                  Please Sign In 
+                </h2>
                 <>
                   <div class="form-floating mb-3">
                     <input
